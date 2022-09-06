0b072a77cc0b45252e1ed491647b87bc0526382d
Nkwo Chukwuemeka <emekankwo49@gmail.com>
2022-09-06 11:36:56 +0100

    created the login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home, List, Hotel } from "./pages";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, List, Hotel, Login } from './pages';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="hotels" element={<List />} />
-        <Route path="hotels/:id" element={<Hotel />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="hotels" element={<List />} />
+                <Route path="hotels/:id" element={<Hotel />} />
+                <Route path="login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
